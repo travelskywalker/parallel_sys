5570f51264cd9dacf8e1d4a42df8a8936b005172
lei_mendenilla <https://lei_mendenilla@bitbucket.org>
2018-02-22 11:25:27 +0800

    front-end push

--- frontend/parallel/src/app/app.module.ts
+++ frontend/parallel/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { MaterializeModule } from 'angular2-materialize';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
+  	RouterModule.forRoot(
+      appRoutes,
+      // { enableTracing: true } // <-- debugging purposes only
+    )
     BrowserModule,
     MaterializeModule
   ],
